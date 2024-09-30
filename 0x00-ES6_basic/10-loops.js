f97cfdffd93a2f6c892435852cb5a7b9dded5d85
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-09-30 22:18:38 +0300

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const answer = [];
   for (const [idx, value] of array.entries()) {
-    answer[idx] = appendString + value;
+    array[idx] = appendString + value;
   }
-  return answer;
+
+  return array;
 }
