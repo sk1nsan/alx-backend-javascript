062daecb6359744dc7c61243dd0eaee3a51000fb
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-02 18:29:54 +0300

    task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,8 @@
+module.exports = function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    }
+    reject(Error('The fake API is not working currently'));
+  });
+};
