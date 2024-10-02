b658d9e4bfdb1b993e0c87f6a3a8584372284f3b
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-02 20:54:21 +0300

    fixed eslint

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,6 +1,4 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise.race([chinaDownload, USDownload])
-    .then((result) => {
-      return result;
-    });
+    .then((result) => result);
 }
