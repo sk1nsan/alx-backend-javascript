4d8e559904442da478acf117bd5ef422e706697e
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-08 17:16:05 +0300

    task 100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    if (weakMap.get(endpoint) > 4) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+}
+
+module.exports = { queryAPI, weakMap };
