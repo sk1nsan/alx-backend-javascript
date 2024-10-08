f032a90e5ee9884cdfb5186bbedc02d4e4796e02
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-08 17:21:24 +0300

    task 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
-    if (weakMap.get(endpoint) > 4) {
+    if (weakMap.get(endpoint) >= 4) {
       throw new Error('Endpoint load is high');
     }
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
