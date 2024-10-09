8e9729d13c8ecfea1798d6fad4836de6bf01c742
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-09 20:55:40 +0300

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,6 +14,27 @@ interface Directors extends Teacher {
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
+
 const printTeacher: printTeacherFunction = function(firstName, lastName) {
   return firstName[0] + "." + lastName;
+}
+
+interface Student {
+  firstName: string,
+  lastName: string,
+}
+
+class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return 'Currently working';
+  }
+  displayName() {
+    return this.firstName;
+  }
 }
