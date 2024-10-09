906e5691e0e51be60498d5bacddfe53b6d7830fd
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-09 20:26:04 +0300

    task 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,16 @@ interface Teacher {
   yearsOfExperience?: number,
   location: string,
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
