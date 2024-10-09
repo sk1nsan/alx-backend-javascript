d198cef38e10f0a5ef60ce9cb5addbfaa9456ccf
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-09 23:26:13 +0300

    task 9

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,22 @@
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'Noah',
+  lastName: 'Ramsi',
+  experienceTeachingC: 10,
+};
+console.log('C++');
+cpp.setTeacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher = cTeacher;
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
