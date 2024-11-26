69d3d47344aa1646c29a1f52812059b2f1245c82
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-26 10:45:15 +0200

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,7 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  console.log('This important software is now closing');
+});
