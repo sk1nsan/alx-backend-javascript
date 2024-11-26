47406d25ee227db222af52caa07104e6ba11d6db
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-26 15:04:45 +0200

    task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,39 +1,47 @@
 const express = require('express');
 const fs = require('fs');
 
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, output) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (output) {
+        let result = '';
+        const students = {};
+        const data = output.trim().split('\n').slice(1);
+        for (const i of data) {
+          const row = i.split(',');
+          if (students[row[3]] === undefined) {
+            students[row[3]] = [row[0]];
+          } else {
+            students[row[3]].push(row[0]);
+          }
+        }
+        result += 'This is the list of our students\n';
+        result += `Number of students: ${data.length}\n`;
+        for (const [key, value] of Object.entries(students)) {
+          result += `Number of students in ${key}: ${value.length}. List: ${value.join(', ')}\n`;
+        }
+        resolve(result.trim());
+      }
+    });
+  });
+}
+
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  let count = -1;
-  const csStudents = [];
-  const sweStudents = [];
-  fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    if (err) {
-      throw (new Error('Cannot load the database'));
-    }
-    if (data) {
-      for (const i of data.split('\n')) {
-        if (i === '') {
-          break;
-        }
-        count += 1;
-        if (i.includes('CS')) {
-          csStudents.push(i.slice(0, i.indexOf(',')));
-        }
-        if (i.includes('SWE')) {
-          sweStudents.push(i.slice(0, i.indexOf(',')));
-        }
-      }
-      res.write('This is the list of our students\n');
-      res.write(`Number of students: ${count}\n`);
-      res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-      res.end();
-    }
-  });
+  countStudents(process.argv[2]).then((value) => {
+    res.end(value);
+  })
+    .catch((err) => {
+      res.end(err.message);
+    });
 });
 
 app.listen(1245);
