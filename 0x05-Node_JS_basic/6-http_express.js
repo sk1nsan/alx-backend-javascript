f3cd6be9d3e0be0477a9cb55cc6007fe09d1dd6b
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-26 12:36:13 +0200

    task 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app = express();
-app.all('/', (res) => {
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.listen(1245);
+module.exports = app;
